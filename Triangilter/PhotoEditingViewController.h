1e66811a7c27837408540d98d789de15ac491d67
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2014-10-14 16:53:06 +0200

    Added photo editing extension draft

---
+++ Triangilter/PhotoEditingViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoEditingViewController.h
+//  Triangilter
+//
+//  Created by Matěj Kašpar Jirásek on 14/10/14.
+//  Copyright (c) 2014 X Production s.r.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoEditingViewController : UIViewController
+
+@end
