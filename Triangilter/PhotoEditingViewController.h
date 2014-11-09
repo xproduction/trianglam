390e34cee6f9a4199d2b5a58af8d24673ca8ea24
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2014-11-09 23:18:45 +0100

    1.1 version

--- Triangilter/PhotoEditingViewController.h
+++ Triangilter/PhotoEditingViewController.h
@@ -6,7 +6,14 @@
 //  Copyright (c) 2014 X Production s.r.o. All rights reserved.
 //
 
+#define BOTTOM_BAR_HEIGHT 80.0
+#define STATUS_BAR_HEIGHT 20.0
+#define TOP_BAR_HEIGHT 44.0
+
+#define IS_IPAD ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad)
+
 #import <UIKit/UIKit.h>
+//#import "CameraViewController.h"
 
 @interface PhotoEditingViewController : UIViewController
 
